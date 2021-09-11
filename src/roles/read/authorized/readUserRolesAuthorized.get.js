90455699515da42a2c4c4e4d6760c2fe4001596d
Will Tesler <willtesler@gmail.com>
2021-09-10 20:37:13 -0500

    ...

--- src/roles/read/authorized/readUserRolesAuthorized.get.js
+++ src/roles/read/authorized/readUserRolesAuthorized.get.js
@@ -2,10 +2,11 @@ const authorizedGet = require('../../../handlers/authorizedGet');
 
 module.exports = authorizedGet(async (req, res, user) => {
   const pageToken = req.query.pageToken;
+  const claim = req.query.claim;
+  const email = req.query.email;
+  const count = req.query.count;
 
   const readUserRoles = require('../readUserRoles');
-  const users = await readUserRoles(pageToken);
-  return {
-    users: users
-  }
+  const response = await readUserRoles(pageToken, claim, email, count);
+  return response;
 }, ['admin']);
--- src/roles/read/readUserRoles.js
+++ src/roles/read/readUserRoles.js
@@ -1,20 +1,50 @@
-module.exports = async (pageToken) => {
+module.exports = async (pageToken, claim = null, email = null, count=50) => {
   const admin = require('firebase-admin');
-
   const auth = admin.auth();
 
-  const listUsersResult = await auth.listUsers(50, pageToken);
+  let users = [];
+  let newPageToken = undefined;
 
-  const users = listUsersResult.users.map((userRecord) => {
-    const data = userRecord.toJSON();
+  if (!count) {
+    count = 50;
+  }
+
+  if (email) {
+    const result = await auth.getUsers([{ email: email }]);
+    users = result.users;
+  } else if (claim) {
+    do {
+      const result = await auth.listUsers(1000, newPageToken);
+      result.users = result.users.filter(userRecord => {
+        const customClaims = userRecord.customClaims;
+        return customClaims && customClaims[claim];
+      });
+      users.push(...result.users);
+      newPageToken = result.pageToken;
+    } while (newPageToken);
+  } else {
+    console.log(count, pageToken);
+    const result = await auth.listUsers(count, pageToken);
+    users = result.users;
+    newPageToken = result.pageToken;
+  }
+
+  if (!newPageToken) {
+    newPageToken = undefined; // firebase accepts undefined but not null.
+  }
+
+  users = users.map((data) => {
     return {
       uid: data.uid,
       displayName: data.displayName,
       photoURL: data.photoURL,
       email: data.email,
-      customClaims: data.customClaims,
+      customClaims: data.customClaims ? data.customClaims : {}
     };
   });
 
-  return users;
+  return {
+    users: users,
+    pageToken: newPageToken
+  };
 };
